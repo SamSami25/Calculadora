e5c6f60ba2c0af2e5c95c4331089df599bfddb2e
Sami Aldaz <saldazc@est.ups.edu.ec>
2022-07-07 11:59:42 -0500

    Fin de la Aplicación

--- principal.cpp
+++ principal.cpp
@@ -13,3 +13,40 @@ Principal::~Principal()
     delete ui;
 }
 
+
+void Principal::on_cmdCalcular_released()
+{
+    calcular();
+}
+
+void Principal::on_actionCalcular_triggered()
+{
+    calcular();
+}
+
+void Principal::calcular()
+{
+    double numero1 = ui->inNum1->value();
+    double numero2 = ui->inNum2->value();
+
+    double resultado = 0.0;
+
+            switch (ui->inOperacion->currentIndex()){
+            case 0:
+                resultado = numero1 + numero2;
+            break;
+            case 1:
+                resultado = numero1 - numero2;
+            break;
+            case 2:
+                resultado = numero1 * numero2;
+            break;
+            case 3:
+                resultado = numero1 / numero2;
+            break;
+            default:
+            break;
+}
+            QMessageBox::information(this,"Resultado","El resultado es: " + QString::number(resultado));
+}
+
--- principal.h
+++ principal.h
@@ -2,6 +2,7 @@
 #define PRINCIPAL_H
 
 #include <QMainWindow>
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Principal; }
@@ -15,7 +16,14 @@ public:
     Principal(QWidget *parent = nullptr);
     ~Principal();
 
+private slots:
+
+    void on_cmdCalcular_released();
+    void on_actionCalcular_triggered();
+
 private:
     Ui::Principal *ui;
+    void calcular();
+
 };
 #endif // PRINCIPAL_H
